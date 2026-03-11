9e9b1dd899489d50ec25cf3694b09e07b85d251b
SkyWarLimit <ihsanbinrahmatw@gmail.com>
2026-03-11 19:29:22 +0700

    Praktikum 2.5 JS 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,19 +6,25 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username'=> 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('123456'),
-                'level_id' => 2
-            ]
-        );
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('123456'),
+            'level_id' => 2,
+        ]);
+        $user->username = 'manager12';
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
